fb913d25a3a427e0527124198d74160ec8b367f0
LKenji <54600289+lucaskmk@users.noreply.github.com>
2025-09-24 14:47:05 -0300

    teste

--- exe4/main.c
+++ exe4/main.c
@@ -4,61 +4,54 @@
 #include "hardware/timer.h"
 #include "hardware/adc.h"
 
-#define PIN_LED_B 4
-#define ADC_CH 2
-#define VREF 3.3f
+#define LED_PIN 4
+#define ADC_PIN 28
+#define ADC_CH  2
+#define VREF    3.3f
 #define ADC_MAX (1 << 12)
-
-typedef struct {
-    int zone;
-    bool state;
-} led_control_t;
 
 static repeating_timer_t timer;
 
-bool timer_callback(repeating_timer_t *rt) {
-    led_control_t *ctrl = (led_control_t *)rt->user_data;
-    if (ctrl->zone == 0) {
-        gpio_put(PIN_LED_B, 0);
-        ctrl->state = false;
-        return true;
-    }
-    ctrl->state = !ctrl->state;
-    gpio_put(PIN_LED_B, ctrl->state);
+bool led_blink_callback(repeating_timer_t *t) {
+    bool state = gpio_get(LED_PIN);
+    gpio_put(LED_PIN, !state);
     return true;
 }
 
 int main() {
-    stdio_init_all();
-    gpio_init(PIN_LED_B);
-    gpio_set_dir(PIN_LED_B, true);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 0);
+
     adc_init();
-    adc_gpio_init(28);
+    adc_gpio_init(ADC_PIN);
     adc_select_input(ADC_CH);
 
-    led_control_t ctrl = {0, false};
-    int last_zone = -1;
+    int current_zone = -1;
 
-    while (1) {
+    while (true) {
         uint16_t raw = adc_read();
         float voltage = raw * VREF / ADC_MAX;
 
-        int new_zone;
-        if (voltage < 1.0f) new_zone = 0;
-        else if (voltage < 2.0f) new_zone = 1;
-        else new_zone = 2;
+        int zone;
+        if (voltage <= 1.0f) {
+            zone = 0;
+        } else if (voltage <= 2.0f) {
+            zone = 1;
+        } else {
+            zone = 2;
+        }
 
-        if (new_zone != last_zone) {
-            last_zone = new_zone;
-            ctrl.zone = new_zone;
+        if (zone != current_zone) {
+            current_zone = zone;
             cancel_repeating_timer(&timer);
-            if (ctrl.zone == 0) {
-                gpio_put(PIN_LED_B, 0);
-                ctrl.state = false;
-            } else if (ctrl.zone == 1) {
-                add_repeating_timer_ms(300, timer_callback, &ctrl, &timer);
+
+            if (zone == 0) {
+                gpio_put(LED_PIN, 0);
+            } else if (zone == 1) {
+                add_repeating_timer_ms(300, led_blink_callback, NULL, &timer);
             } else {
-                add_repeating_timer_ms(500, timer_callback, &ctrl, &timer);
+                add_repeating_timer_ms(500, led_blink_callback, NULL, &timer);
             }
         }
     }
